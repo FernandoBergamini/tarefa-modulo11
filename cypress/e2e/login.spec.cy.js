a9a667153103334bd39532527f248eaf82a95dfe
Fernando Bergamini <fernandobr.89@hotmail.com>
2022-09-14 19:40:18 -0300

    alterações

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
 
 context('Funcionalidade login', () => {
 
     beforeEach(() => {
-      cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+      cy.visit ('minha-conta')
     });
   
     afterEach(() => {
@@ -19,6 +20,25 @@ context('Funcionalidade login', () => {
   
       
     });
+
+    it('Deve fazer login com sucesso - usando arquivo de dados', () => {
+       
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain' , 'Minha conta')
+          
+     });
+
+     it.only('Deve fazer login com sucesso - usando fixture', () => {
+       
+        cy.get('#username').type(dados.usuario)
+        cy.get('#password').type(dados.senha, {log:false})
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain' , 'Minha conta')
+          
+     });
+  
   
     it('Deve exibir msg de erro ao inserir usuario invalidos', () => {
       
--- cypress/e2e/pre_cadastro.spec.cy.js
+++ cypress/e2e/pre_cadastro.spec.cy.js
@@ -4,7 +4,7 @@ var faker = require('faker')
 describe('funcionalidade de pre cadastro', () => {
 
 beforeEach(() => {
-    cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.visit ('minha-conta/')
 });
 
 it('Deve completar o Pré Cadastro com Sucesso', () => {
